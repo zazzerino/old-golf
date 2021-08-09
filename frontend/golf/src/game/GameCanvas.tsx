0eeeb60138c26be194ce76080c30dfda183e380a
Kyle Patterson <patterkyle@gmail.com>
2021-08-09 13:42:29 -0500

    click player hand

--- frontend/golf/src/game/GameCanvas.tsx
+++ frontend/golf/src/game/GameCanvas.tsx
@@ -15,13 +15,25 @@ interface Size {
   height: number;
 }
 
+interface HandOpts {
+  clickedCard?: ClickedCard;
+  onClick?: (index: number) => void;
+}
+
+interface DrawCardOpts {
+  highlight?: boolean;
+  onClick?: (card: string) => void;
+}
+
+type HandPos = 'bottom' | 'left' | 'top' | 'right';
+
 const svgNS = 'http://www.w3.org/2000/svg';
 const xlinkNS = 'http://www.w3.org/1999/xlink';
 
 const cardScale = '10%';
 const cardSize: Size = { width: 60, height: 84 };
 
-const hlPadding = 1;
+const hlPadding = 1; // how far highlight rect extends past card
 const handPadding = 2;
 
 function empty(elem: Element) {
@@ -32,6 +44,7 @@ function empty(elem: Element) {
 
 function makeRect(coord: Coord, size: Size, color = '#44ff00') {
   const rect = document.createElementNS(svgNS, 'rect');
+
   rect.setAttribute('x', coord.x.toString());
   rect.setAttribute('y', coord.y.toString());
   rect.setAttribute('width', size.width.toString());
@@ -41,7 +54,7 @@ function makeRect(coord: Coord, size: Size, color = '#44ff00') {
   return rect;
 }
 
-function makeCard(card: string, coord: Coord, onClick = (_card: string) => {}) {
+function makeCard(card: string, coord: Coord, onClick = (card: string) => {}) {
   const img = document.createElementNS(svgNS, 'image');
 
   img.setAttribute('width', cardScale);
@@ -54,22 +67,17 @@ function makeCard(card: string, coord: Coord, onClick = (_card: string) => {}) {
   return img;
 }
 
-function makeCardHighlight(coord: Coord) {
-    const x = coord.x - hlPadding;
-    const y = coord.y - hlPadding;
-    const hlCoord = { x, y };
+function makeHighlight(coord: Coord) {
+  const x = coord.x - hlPadding;
+  const y = coord.y - hlPadding;
+  const hlCoord = { x, y };
 
-    const width = cardSize.width + hlPadding * 2;
-    const height = cardSize.height + hlPadding * 2;
-    const hlSize = { width, height };
+  const width = cardSize.width + hlPadding * 2;
+  const height = cardSize.height + hlPadding * 2;
+  const hlSize = { width, height };
 
-    const rect = makeRect(hlCoord, hlSize);
-    return rect;
-}
-
-interface DrawCardOpts {
-  onClick?: (card: string) => void;
-  highlight?: boolean;
+  const rect = makeRect(hlCoord, hlSize);
+  return rect;
 }
 
 function drawCard(svg: SVGElement, card: string, coord: Coord, opts: DrawCardOpts = {}) {
@@ -77,7 +85,7 @@ function drawCard(svg: SVGElement, card: string, coord: Coord, opts: DrawCardOpt
   const img = makeCard(card, coord, onClick);
 
   if (highlight === true) {
-    const rect = makeCardHighlight(coord);
+    const rect = makeHighlight(coord);
     svg.appendChild(rect);
   }
 
@@ -110,31 +118,40 @@ function tableCardClicked() {
 function drawTableCard(svg: SVGElement, size: Size, card: string, clickedCard: ClickedCard) {
   const coord = tableCardCoord(size);
   const highlight = clickedCard === 'table';
-  const onClick = tableCardClicked;
+  const opts = { highlight, onClick: tableCardClicked }
 
-  drawCard(svg, card, coord, { highlight, onClick });
+  drawCard(svg, card, coord, opts);
 }
 
-function makeHand(cards: string[]) {
+function makeHand(cards: string[], opts: HandOpts) {
+  const { clickedCard, onClick } = opts;
   const group = document.createElementNS(svgNS, 'g');
-  const spacing = 2;
 
   for (let i = 0; i < cards.length; i++) {
     const inTopRow = i < 3;
     const offset = inTopRow ? i : i - 3;
-    const x = cardSize.width * offset + spacing * offset;
-    const y = inTopRow ? 0 : cardSize.height + spacing;
+    const x = cardSize.width * offset + handPadding * offset;
+    const y = inTopRow ? 0 : cardSize.height + handPadding;
+    const coord = { x, y };
 
-    const card = makeCard(cards[i], { x, y });
+    const card = makeCard(cards[i], coord);
+
+    if (clickedCard === i) {
+      const hlRect = makeHighlight(coord);
+      group.appendChild(hlRect);
+    }
+
+    if (onClick) {
+      card.onclick = () => onClick(i);
+    }
+
     group.appendChild(card);
   }
 
   return group;
 }
 
-type HandPos = 'bottom' | 'left' | 'top' | 'right';
-
-function drawHand(svg: SVGElement, cards: string[], pos: HandPos) {
+function drawHand(svg: SVGElement, cards: string[], pos: HandPos, opts: HandOpts = {}) {
   const boundingRect = svg.getBoundingClientRect();
   const canvasWidth = boundingRect.width;
   const canvasHeight = boundingRect.height;
@@ -142,7 +159,7 @@ function drawHand(svg: SVGElement, cards: string[], pos: HandPos) {
   const midX = canvasWidth / 2 - cardSize.width * 1.5;
   const bottomY = canvasHeight - cardSize.height * 2 - handPadding * 2;
 
-  const hand = makeHand(cards);
+  const hand = makeHand(cards, opts);
 
   switch (pos) {
     case 'bottom':
@@ -156,6 +173,15 @@ function drawHand(svg: SVGElement, cards: string[], pos: HandPos) {
   svg.appendChild(hand);
 }
 
+function playerHandClicked(index: ClickedCard) {
+  store.dispatch(cardClicked(index));
+}
+
+function drawPlayerHand(svg: SVGElement, cards: string[], clickedCard: ClickedCard = null) {
+  const onClick = (i: number) => playerHandClicked(i as ClickedCard);
+  drawHand(svg, cards, 'bottom', { clickedCard, onClick });
+}
+
 function drawGame(svg: SVGElement, size: Size, game: Game, clickedCard: ClickedCard) {
   drawDeck(svg, size);
 
@@ -163,14 +189,16 @@ function drawGame(svg: SVGElement, size: Size, game: Game, clickedCard: ClickedC
     drawTableCard(svg, size, game.tableCard, clickedCard);
   }
 
-  drawHand(svg, ['AC', '2C', '3C', '4H', '5H', '6H'], 'bottom');
+  drawPlayerHand(svg, ['AC', '2C', '3C', '4H', '5H', '6H'], clickedCard);
   drawHand(svg, ['AC', '2C', '3C', '4H', '5H', '6H'], 'top');
 }
 
 export function GameCanvas() {
   const className = 'GameCanvas';
+
   const svgRef = React.useRef<SVGSVGElement>(null);
   const size: Size = { width: 600, height: 500 };
+
   const game = useAppSelector(selectCurrentGame);
   const clickedCard = useAppSelector(selectClickedCard);
 
